70688345892fcc3153d26be82fc5b680d69a975b
IFEE09 <ivanfaridespadas@gmail.com>
2022-11-26 20:11:20 -0600

    cambiando nombre de las carpetas

---
+++ primera-unidad/2-ciclos/ciclos.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, numero = 0; 
+    printf("Dime cuanto vale n: ");
+    scanf("%d", &n); 
+
+    for(int  i = 0; i < n; i++) {
+        numero++;
+    }
+
+    printf("j vale %d con el ciclo for\n", numero);
+
+    numero = 0; //aquí reinicia el número de nuevo para que entre al siguiente ciclo
+
+    while(numero < n) {
+        numero++;
+    }
+
+     printf("j vale %d con el ciclo while\n", numero);
+
+    return 0;
+}
