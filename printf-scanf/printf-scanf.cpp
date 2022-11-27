466a617d0166f4bb980fbaba2187d97ce2daef27
IFEE09 <ivanfaridespadas@gmail.com>
2022-11-26 19:34:05 -0600

    agregando comentarios al código

--- printf-scanf/printf-scanf.cpp
+++ printf-scanf/printf-scanf.cpp
@@ -7,5 +7,7 @@ int main() {
     scanf("%s", nombre);
     printf("Hola %s\n", nombre);
 
+    //Para leer cadenas con espacios y dos nombres, usar fgets -> https://parzibyte.me/blog/2020/12/30/c-leer-cadena-espacios/
+
     return 0;
 }
