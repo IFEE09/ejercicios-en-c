f155ff578b36ebeeb93b38ed490f8acbfaafb721
IFEE09 <ivanfaridespadas@gmail.com>
2022-11-26 20:02:45 -0600

    agregando resolucion parcial del segundo ejercicio

--- primera-unidad/ciclos/ciclos.cpp
+++ primera-unidad/ciclos/ciclos.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main() {
+
+    int n, numero = 0; 
+    printf("Dime cuanto vale n: ");
+    scanf("%d", &n); 
+
+    for(int  i = 0; i < n; i++) {
+        numero++;
+    }
+
+    printf("j vale %d con el ciclo for\n", numero);
+
     return 0;
 }
